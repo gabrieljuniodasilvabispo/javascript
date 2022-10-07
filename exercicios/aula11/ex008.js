eb20abf0bfc5dfd8f284c30136f657798c45bc61
gabrieljuniodasilvabispo <gabrieljn080@gmail.com>
2022-10-07 10:48:18 -0300

    ex 9, 10 e 11 de js

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60) { // Condição simples
+    console.log('Você ultraapassou a velocidade permitida. Você está multado!!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança.')
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,10 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+
+if(país != 'Brasil') {
+    console.log("Você é estrangeiro")
+}
+
+else {
+    console.log('Você é brasileiro')
+}
