e07b334c88bdae9fc33ace9b31d32e753194957b
gabrieljuniodasilvabispo <gabrieljn080@gmail.com>
2022-10-15 21:36:29 -0300

    praticando js

---
+++ praticando/ex001/script.js
@@ -0,0 +1,8 @@
+var fundo1 = document.getElementById('black')
+var fundo2 = document.getElementById('div#blue')
+var corpo = window.document.body
+
+function mudaCor(){
+    fundo1.style.background = 'black'
+    corpo.style.background = 'black'
+}
