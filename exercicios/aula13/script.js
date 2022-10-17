ba90670e35d1743a035cb5c26a567ea29975cce9
gabrieljuniodasilvabispo <gabrieljn080@gmail.com>
2022-10-17 09:48:08 -0300

    att de js

---
+++ exercicios/aula13/script.js
@@ -0,0 +1,17 @@
+/*
+var contador = 1
+
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+while (contador <= 6)
+    
