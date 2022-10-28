a140574721cd33bde252db19c5ab1dcc0bc63dd6
gabriejuniodasilvabispo <gabrieljn080@gmail.com>
2022-10-28 09:46:10 -0300

    inicio em objetos

---
+++ exercicios/aula17/objeto01.js
@@ -0,0 +1,11 @@
+let amigo = {
+    nome: 'José', 
+    sexo: 'M', 
+    peso: 85.4,
+    engordar(p) {
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+amigo.engordar(2)
+console.log(`O ${amigo.nome} pesa ${amigo.peso}`)
