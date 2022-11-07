f09a777321b37a711b60fd78a00bffb7eba8fd4f
gabriejuniodasilvabispo <gabrieljn080@gmail.com>
2022-11-07 08:57:09 -0300

    att js

--- praticando/funçoes/funçao002.js
+++ praticando/funçoes/funçao002.js
@@ -8,4 +8,4 @@ function escreverEndereco (rua, cidade, estado, ...complementos) {
 
 escreverEndereco('Rua do Registro', 'Contagem', 'Estado de MG', 'NÚMERO 364', 'CASA 3')
 
-/* Perceba que depois do que não declaramos como parámetro tudo foi armazenado como um array. Essa é função de uma function cujo número de parâmetros você não sabe. Damos o nome a isso de variáel rest.*/
+/* Perceba que depois do que não declaramos como parámetro tudo foi armazenado como um array. Essa é função de uma function cujo número de parâmetros você não sabe. Damos o nome a isso de variáel rest(...).*/
---
+++ praticando/funçoes/funçao007.js
@@ -0,0 +1,7 @@
+let temperatura = 10
+
+let temp2 = function (a) {
+    return a * 9/5 + 32; 
+}
+
+console.log(`A temperatura está em ${temperatura} graus celsius, o equivalente a ${temp2(temperatura)} em Farenheit`)
