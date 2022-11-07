e55a67bc519cb501307537dc2b5d251d24cf4683
gabriejuniodasilvabispo <gabrieljn080@gmail.com>
2022-11-07 10:16:55 -0300

    att js

--- praticando/funçoes/funçao007.js
+++ praticando/funçoes/funçao007.js
@@ -1,7 +1,7 @@
 let temperatura = 10
 
-let temp2 = function (a) {
-    return a * 9/5 + 32; 
+let temp2 = function (t) {
+    return t * 9/5 + 32; 
 }
 
 console.log(`A temperatura está em ${temperatura} graus celsius, o equivalente a ${temp2(temperatura)} em Farenheit`)
