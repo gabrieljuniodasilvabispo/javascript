f9c364560c228d08fc46b0fe901b839b58adce65
gabrieljuniodasilvabispo <gabrieljn080@gmail.com>
2022-12-27 09:26:34 -0300

    att js metodo every

--- cfbCursos/aula026/aula026.js
+++ cfbCursos/aula026/aula026.js
@@ -1,18 +1,19 @@
 const p_array = document.querySelector("#array")
-const txt_search = document.querySelector("#txt_search")
-const btnSearch = document.querySelector("#btnSearch")
+const btnVerify = document.querySelector("#verify")
 const result = document.querySelector("#result")
 
-const elementsArray = [2, 4, 8, 20 ,100, 15, 24]
+const elementsArray = [25, 4, 8, 20 ,100, 15, 24]
 
 p_array.innerHTML = `[${elementsArray}]`
 
-btnSearch.addEventListener("click",(evt)=>{
-    result.innerHTML = 'Valor não encontrado'
-    const ret = elementsArray.find((el, i)=>{
-        if(el == txt_search.value){
-            result.innerHTML = `Valor pesquisado ${el} na posição ${i}`
-            return el
+btnVerify.addEventListener("click",(evt)=>{
+    const ret = elementsArray.every((el,i)=>{
+        if(el <= 18){
+            result.innerHTML = `Array não conforme na posição ${i}`
         }
+        return el > 18
     })
+    if(ret){
+        result.innerHTML = 'Array OK!'
+    }
 })
