ad599d354bd06669e2465d7665bcf615dc0c4878
gabrieljuniodasilvabispo <gabrieljn080@gmail.com>
2022-12-29 09:52:44 -0300

    att js profissional calculadora simples

---
+++ cfbCursos/aula028/aula028.js
@@ -0,0 +1,37 @@
+const valor1 = document.querySelector("#valor1")
+const valor2 = document.querySelector("#valor2")
+const soma = document.querySelector("#soma")
+const subtracao = document.querySelector("#subtracao")
+const multiplicacao = document.querySelector("#multiplicacao")
+const divisao = document.querySelector("#divisao")
+let res = document.querySelector("#res")
+
+let numeros = [valor1.value, valor2.value]
+
+let op = [
+    (v1, v2)=>{
+        return Number(v1) + Number(v2)
+    },
+    (v1, v2)=>{
+        return Number(v1) - Number(v2)
+    },
+    (v1, v2)=>{
+        return Number(v1) * Number(v2)
+    },
+    (v1, v2)=>{
+        return Number(v1) / Number(v2)
+    }
+]
+
+soma.addEventListener("click",(evt)=>{
+    res.innerHTML = op[0](valor1.value, valor2.value)
+})
+subtracao.addEventListener("click",(evt)=>{
+    res.innerHTML = op[1](valor1.value, valor2.value)
+})
+multiplicacao.addEventListener("click",(evt)=>{
+    res.innerHTML = op[2](valor1.value, valor2.value)
+})
+divisao.addEventListener("click",(evt)=>{
+    res.innerHTML = op[3](valor1.value, valor2.value)
+})
