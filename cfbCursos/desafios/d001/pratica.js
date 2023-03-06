8aa53ecbdfd1eeb1bb47b550cab6d6bd9bf710cf
gabrieljuniodasilvabispo <gabrieljn080@gmail.com>
2023-03-06 10:27:34 -0300

    att pratica de classes d001

--- cfbCursos/desafios/d001/pratica.js
+++ cfbCursos/desafios/d001/pratica.js
@@ -10,7 +10,7 @@ let bolas = []
 let numBolas = 0
 
 class Bola{
-    constructor(arrayBolas){
+    constructor(arrayBolas, palco){
         this.tamanho = Math.floor(Math.random()*10)+10
         this.r= Math.floor(Math.random()*255)
         this.g = Math.floor(Math.random()*255)
@@ -21,6 +21,39 @@ class Bola{
         this.vely = Math.floor(Math.random()*2)+0.5
         this.dirx = (Math.random()*10) > 5 ? 1 : -1
         this.diry = (Math.random()*10) > 5 ? 1 : -1
+        this.palco = palco
+        this.arrayBolas = arrayBolas
+        this.id = Date.now()+"_"+Math.floor(Math.random()*10000000000000000)
+        this.desenhar()
+        this.controle = setInterval(this.controlar(),10)
+        this.eu = document.getElementById(this.id)
+        numBolas++
+        num_objetos.innerHTML = numBolas
+    }
+    minhaPos = ()=>{
+        return this.arrayBolas.indexOf(this)
+    }
+    removerBola = ()=>{
+        clearInterval(this.controle)
+        bolas = bolas.filter((b)=>{
+            if(b.id!=this.id){
+                return b
+            }
+        })
+        this.eu.remove()
+        numBolas--
+        num_objetos.innerHTML = numBolas
+    }
+    desenhar = ()=>{
+        const div = document.createElement("div")
+        div.setAttribute("id", this.id)
+        div.setAttribute("class", "bola")
+        div.setAttribute("style", `left:${this.px};top:${this.py};width:${this.tamanho};height:${this.tamanho};background-color:rgb(${this.r},${this.g},${this.b})`)
+        this.palco.appendChild(div)
+    }
+
+    controlar = ()=>{
+
     }
 }
 
@@ -33,8 +66,6 @@ btn_add.addEventListener("click",(evt)=>{
     const qntd = txt_qntd.value
     for(let i=0; i < qntd; i++){
         const bolinha = new Bola()
-        const div = document.createElement("div")
-        
     }
 })
 
